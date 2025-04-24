e01c655e948dd5c3b7eccfa29ced49c1897625ea
Eldos <eldos.baktybekov@gmail.com>
2025-04-24 23:48:57 +0600

    Refactor save_food_menu_items method to improve readability and maintainability

--- src/infrastructure/adapters/food_menu_items.py
+++ src/infrastructure/adapters/food_menu_items.py
@@ -19,51 +19,51 @@ class FoodMenuItemGateway:
         self,
         daily_food_menus: Iterable[DailyFoodMenu],
     ) -> None:
-        async with self.session.begin():
-            items_to_update: list[FoodMenuItemModel] = []
-            items_to_add: list[FoodMenuItemModel] = []
-            for new_daily_food_menu in daily_food_menus:
-                old_daily_food_menu = (
-                    await self.get_latest_food_menu_items_for_date(
+        items_to_update: list[FoodMenuItemModel] = []
+        items_to_add: list[FoodMenuItemModel] = []
+        for new_daily_food_menu in daily_food_menus:
+            old_daily_food_menu = (
+                await self.get_latest_food_menu_items_for_date(
+                    date=new_daily_food_menu.at,
+                )
+            )
+            old_item_name_to_item = {
+                item.name: item for item in old_daily_food_menu.items
+            }
+            new_item_name_to_item = {
+                item.name: item for item in new_daily_food_menu.items
+            }
+
+            inactive_item_names = set(old_item_name_to_item) - set(
+                new_item_name_to_item
+            )
+            new_item_names = set(new_item_name_to_item) - set(
+                old_item_name_to_item
+            )
+            for inactive_item_name in inactive_item_names:
+                item = old_item_name_to_item[inactive_item_name]
+                items_to_update.append(
+                    FoodMenuItemModel(
+                        name=item.name,
+                        calories_count=item.calories_count,
+                        photo_url=item.photo_url,
+                        date=new_daily_food_menu.at,
+                        is_active=False,
+                    )
+                )
+
+            for new_item_name in new_item_names:
+                item = new_item_name_to_item[new_item_name]
+                items_to_add.append(
+                    FoodMenuItemModel(
+                        name=item.name,
+                        calories_count=item.calories_count,
+                        photo_url=item.photo_url,
                         date=new_daily_food_menu.at,
                     )
                 )
-                old_item_name_to_item = {
-                    item.name: item for item in old_daily_food_menu.items
-                }
-                new_item_name_to_item = {
-                    item.name: item for item in new_daily_food_menu.items
-                }
 
-                inactive_item_names = set(old_item_name_to_item) - set(
-                    new_item_name_to_item
-                )
-                new_item_names = set(new_item_name_to_item) - set(
-                    old_item_name_to_item
-                )
-                for inactive_item_name in inactive_item_names:
-                    item = old_item_name_to_item[inactive_item_name]
-                    items_to_update.append(
-                        FoodMenuItemModel(
-                            name=item.name,
-                            calories_count=item.calories_count,
-                            photo_url=item.photo_url,
-                            date=new_daily_food_menu.at,
-                            is_active=False,
-                        )
-                    )
-
-                for new_item_name in new_item_names:
-                    item = new_item_name_to_item[new_item_name]
-                    items_to_add.append(
-                        FoodMenuItemModel(
-                            name=item.name,
-                            calories_count=item.calories_count,
-                            photo_url=item.photo_url,
-                            date=new_daily_food_menu.at,
-                        )
-                    )
-
+        async with self.session.begin():
             if items_to_add:
                 self.session.add_all(items_to_add)
             if items_to_update:
